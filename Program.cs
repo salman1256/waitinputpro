07529615a1ec7a0b315764a57ba325997e295d49
salman1256 <salman1256@yahoo.com>
2025-01-24 23:31:43 +0530

    Final Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Check if a username was provided as an argument
+            if (args.Length > 0)
+            {
+                string username = args[0];
+                Console.WriteLine($"The entered username is: {username}");
+            }
+            else
+            {
+                Console.WriteLine("No username provided.");
+            }
+        }
+    }
+}
